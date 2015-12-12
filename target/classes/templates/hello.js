81715355ab88b6dc358a40fd5c42457ec77b4430
Jaspreet <jjuneja@beachbody.com>
2015-12-12 19:09:31 +0530

    removing target folder

--- target/classes/templates/hello.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/api/places/list"
-    }).then(function(data) {
-        $("#lat").val(data[0].latitude);
-        $("#long").val(data[0].longitude);
-    });
-});
-
